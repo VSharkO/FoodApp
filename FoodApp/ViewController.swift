2d8725e6eedbe90e7d8b031ac43e46b8c235a5d2
Valentin Šarić <valentin.saric1@gmail.com>
2018-10-14 15:02:08 +0200

    Initial Commit

---
+++ FoodApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FoodApp
+//
+//  Created by Valentin Šarić on 14/10/2018.
+//  Copyright © 2018 Valentin Šarić. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
